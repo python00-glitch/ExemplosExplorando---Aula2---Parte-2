343b5cb2c46eb0eaebee2d807f6d50a8c96baf9b
Ricardo Pardim Silva <ricardojunior865@gmail.com>
2023-07-28 11:49:32 -0300

    Sync

--- Models/Vendas.cs
+++ Models/Vendas.cs
@@ -1,25 +1,26 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Threading.Tasks;
-// using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
-// namespace ExemplosExplorando___Aula2___Parte_2.Models
-// {
-//     public class Vendas
-//     {
-//         public Vendas (int id, string produto, decimal preco, DateTime dataVenda)
-//         {
-//             Produto = produto;
-//             Preco = preco;
-//             Id = id;
-//             DataVenda = dataVenda;
-//         }
+namespace ExemplosExplorando___Aula2___Parte_2.Models
+{
+    public class Vendas
+    {
+        public Vendas (int id, string produto, decimal preco, DateTime dataVenda, decimal? desconto)
+        {
+            Produto = produto;
+            Preco = preco;
+            Id = id;
+            DataVenda = dataVenda;
+            Desconto = desconto;
+        }
 
-//         public int Id { get; set; }
-//         [JsonProperty("Nome_Produto")]
-//         public string Produto { get; set; }
-//         public decimal Preco { get; set; }
-//         public DateTime DataVenda{ get; set; }
-//     }
-// }
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+        public DateTime DataVenda{ get; set; }
+        public decimal? Desconto { get; set; }
+    }
+}
